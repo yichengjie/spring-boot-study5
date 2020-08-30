dd7fec17a730e137b4d1d18d4f48bda331725d18
易成杰 <626659321@qq.com>
2020-08-30 20:03:36 +0800

    保存

--- mybatis-springboot-study/src/test/java/com/yicj/study/mapper/UserMapperTest.java
+++ mybatis-springboot-study/src/test/java/com/yicj/study/mapper/UserMapperTest.java
@@ -5,7 +5,6 @@ import com.yicj.study.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
