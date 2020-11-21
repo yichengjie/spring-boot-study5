c6fb8d701ac1c964d588421472c4ac4c0d79bbbd
易成杰 <626659321@qq.com>
2020-11-21 18:05:35 +0800

    保存

--- hello-web/src/main/java/com/yicj/study/controller/HomeController.java
+++ hello-web/src/main/java/com/yicj/study/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.yicj.study.controller;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HomeController {
 
     @PostMapping("/hello")
     @ResponseBody
-    public String hello(String id, String name){
-        return "id = " + id +", name : " + name ;
+    public User hello(@RequestBody  User user){
+        return user;
+    }
+
+    @Data
+    static class User{
+        private String id ;
+        private String name ;
     }
 }
