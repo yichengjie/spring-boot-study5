8d53a362990afc71e1152169f336bbb702c0377a
易成杰 <626659321@qq.com>
2020-11-29 10:52:10 +0800

    保存

---
+++ mybatis-springboot-study/src/test/java/com/yicj/study/mapper/com/yicj/study/MyBatisTest.java
@@ -0,0 +1,26 @@
+package com.yicj.study.mapper.com.yicj.study;
+
+import com.yicj.study.entity.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisTest {
+
+    @Test
+    public void test1() throws IOException {
+        String resource = "mybatis-config-test.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        String  statementId = "com.yicj.study.mapper.UserMapper.selectById" ;
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            User user = session.selectOne(statementId, 1);
+            System.out.println(user);
+        }
+    }
+}
