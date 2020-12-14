23cbc17418f8083246d2053b0cd55fa4f5b8fc02
易成杰 <626659321@qq.com>
2020-12-14 21:58:57 +0800

    保存

---
+++ condition-study/src/main/java/com/yicj/study/conditionalonbean/ConditionalOnBeanApp.java
@@ -0,0 +1,12 @@
+package com.yicj.study.conditionalonbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//https://blog.csdn.net/forezp/article/details/84313907
+@SpringBootApplication
+public class ConditionalOnBeanApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConditionalOnBeanApp.class, args) ;
+    }
+}
