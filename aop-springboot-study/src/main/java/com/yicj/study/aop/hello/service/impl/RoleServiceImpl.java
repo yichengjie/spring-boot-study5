5f5139bd1913d063b333d6ad725c33e8f16683e7
易成杰 <626659321@qq.com>
2020-12-20 20:51:06 +0800

    保存

--- aop-springboot-study/src/main/java/com/yicj/study/aop/hello/service/impl/RoleServiceImpl.java
+++ aop-springboot-study/src/main/java/com/yicj/study/aop/hello/service/impl/RoleServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
+@Service("roleService")
 public class RoleServiceImpl implements RoleService {
 
     @Autowired
